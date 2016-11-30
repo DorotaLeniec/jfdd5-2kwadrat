5697e46ca0feafdc68d26982fb06a2c57039d316
Kuba Melson <kubamelson@gmail.com>
2016-11-30 16:37:02 +0100

    Ready hamburger whith CSS and jQuery

--- scripts/hamburger_hiding.js
+++ scripts/hamburger_hiding.js
@@ -3,7 +3,5 @@ $(document).ready(function() {
   $(".menuOption").click(function () {
     var checkBoxes = $(".main-menu-switch");
     checkBoxes.prop("checked", !checkBoxes.prop("checked"));
-    $(this).parent().slideToggle("normal");
-
   });
 });
