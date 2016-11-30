513ec6d5f6231b3c099734311638339ac934b9a5
Maciej Gerka <maciej.gerka@gmail.com>
2016-11-30 17:00:41 +0100

    commit

--- Maciej--FD52K-8_rotacja_ficzerów/javaScript.js
+++ Maciej--FD52K-8_rotacja_ficzerów/javaScript.js
@@ -1,1 +1,11 @@
-$('.feature-1').hide();
+// $('.feature-1').hide();
+//
+// $('.feature-2').css({
+//   position: 'absolute',
+//   left: 169,
+//   top: 182
+// }).animate({left: 230});
+
+$(".features").find("div");
+$(".feature-2").prependTo(".feature-4");
+// $(".feature-2").moveRig
