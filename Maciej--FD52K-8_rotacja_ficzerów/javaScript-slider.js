024f14c9bccb824201cd8ded7004f6995cd01cb9
Maciej Gerka <maciej.gerka@gmail.com>
2016-12-01 16:51:16 +0100

    js do slidera z książki o JS

--- Maciej--FD52K-8_rotacja_ficzerów/javaScript-slider.js
+++ Maciej--FD52K-8_rotacja_ficzerów/javaScript-slider.js
@@ -7,5 +7,43 @@
 // }).animate({left: 230});
 
 // $(".features").find("div");
-$(".feature-4").prependTo(".features");
+// $(".feature-4").prependTo(".features");
 /* $(".feature-2").moveRig*/
+
+$('slider').each(function() {
+  var $this = $(this);
+  var $group = $this.find('.feature-group');
+  var $slides = $this.find('cecha');
+  var buttonArray = [];
+  var currentIndex = 0;
+  var timeout;
+
+
+  // tutaj wpisac move()
+
+  function advance() {
+    clearTimeout(timeout);
+    timeout = setTimeout(function() {
+      if (currentIndex < ($slides.length - 1)) {
+        move (currentIndex + 1);
+      } else {
+        move(0);
+      }
+    }, 4000);
+  }
+
+
+  $.each($slides, function(index) {
+    var $button = $('<button type="button" class="slide-btn">&bull;</button>');
+    if (index === currentIndex) {
+      $button.addClass('active');
+    }
+    $button.on('clock', function() {
+      move(index);
+    }).appendTo('.feature-buttons');
+    buttonArray.push($button);
+  });
+
+  advance();
+
+});
