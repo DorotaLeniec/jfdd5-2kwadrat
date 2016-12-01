ab5d12ea4b4763ae3464bb111d33019d16f13337
Stowarzyszenie <maciej.gerka@gmail.com>
2016-12-01 20:10:49 +0100

    dodanie z książki funkcji move()

--- Maciej--FD52K-8_rotacja_ficzerów/javaScript-slider.js
+++ Maciej--FD52K-8_rotacja_ficzerów/javaScript-slider.js
@@ -18,8 +18,30 @@ $('slider').each(function() {
   var currentIndex = 0;
   var timeout;
 
+  function move(newIndex) {
+      var animateLeft, slideLeft;
+      advance();
+      if ($group.is(’:animated’) || currentIndex === newIndex) {
+          return;
+      }
+      buttonArray[currentIndex].removeClass(’active’);
+      buttonArray[newIndex].addClass(’active’);
 
-  // tutaj wpisac move()
+      if (newIndex > currentIndex) {
+          slideLeft = ’100%’;
+          animateLeft = ’-100%’;
+      } else {
+          slideLeft = ’-100%’;
+          animateLeft = ’100%’;
+      }
+      $slides.eq(newIndex).css( {left: slideLeft, display: ’block’} );
+      $group.animate( {left: animateLeft} , function() {
+          $slides.eq(currentIndex).css( {display: ’none’} );
+          $slides.eq(newIndex).css( {left: 0} );
+          $group.css( {left: 0} );
+          currentIndex = newIndex;
+      });
+  }
 
   function advance() {
     clearTimeout(timeout);
