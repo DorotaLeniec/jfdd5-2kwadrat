b1d6e09a0e007a2b2af50c898a1d27d7cdf5a28e
Dorota <dorleniec@gmail.com>
2016-12-07 00:16:53 +0100

    adding function changing face
    need to specify selectro to pick first with no class FaceWin

--- Game/javascript/kuba-on-click.js
+++ Game/javascript/kuba-on-click.js
@@ -24,30 +24,49 @@ function assigning(){
         var temp;
         // for (var i = 1;i < tablicaSkladnikow.length;){
         random = Math.ceil(Math.random() * 12);
-        console.log(random);
+        // console.log(random);
         temp = tablicaSkladnikow[random];
-        console.log(random);
+        // console.log(random);
         var $foodItem = $('<div>').addClass('food-icon').addClass(temp).attr('foodType', temp);
         $(this).append($foodItem);
     });
 }
 assigning();
 // **********---------------------------On icon click function definition--------------------------*********
+// ------------------------------------zmienne globalne-------------------------
+var emptyPlateCounter = [];
+
 $('.food-icon').click(function () {
     var $firstEmptyPlate = $('.plate:empty:visible').first();
     if ($firstEmptyPlate.hasClass($(this).attr('foodType'))) {
+        emptyPlateCounting($firstEmptyPlate);
         $firstEmptyPlate.append(this).css("opacity","1");
+        console.log(emptyPlateCounter.length);
     }else{
-
         $(this).fadeTo("fast", 0);
         $(this).fadeTo("slow", 1);
     }
     checkPlate();
 });
+
+
+function emptyPlateCounting(firstPlate){
+    emptyPlateCounter.push(firstPlate);
+    console.log("z counting" , firstPlate);
+    if(emptyPlateCounter.length === 1) {
+        $('.face:first').removeClass('sadFace').addClass('smileFace');
+    }else if (emptyPlateCounter.length === 2){
+        $('.face:first').removeClass('smileFace').addClass('happyFace');
+    }else if (emptyPlateCounter.length === 3){
+        $('.face:first').removeClass('happyFace').addClass('winFace').addClass('faceWon');
+        emptyPlateCounter = [];
+    }
+}
 //***----------------------------------Checking Plate function ------------------------------***************
 function checkPlate() {
     $('.table--plate').each(function () {
         if ($(this).find('.plate').length === $(this).find('.food-icon').length) {
+            emptyPlateCounter = [];
             $(this).fadeOut(300);
             $('.skladnik:empty:first').append(
                 $(this).find('.food-icon:first')
@@ -62,3 +81,5 @@ function checkPlate() {
         }
     });
 }
+$('.face:first').css("background-image" , url = '../Images/faces/sad.svg');
+// $('.face').css("background" , "red");
